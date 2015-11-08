94aaef0a36c727b996ca9c5d8a62c041577f551c
ChGreb <Christian.Greb@hhu.de>
2015-11-09 00:26:45 +0100

    hw2 done

--- logmap.cxx
+++ logmap.cxx
@@ -1,34 +1,38 @@
-/* Homework2 
+/* Homework2
  * Chrstian Greb 2030036
-*/
+ */
 #include <iostream>
 #include <fstream>
 
-using namespace std;
+ using namespace std;
 
-int main(){
-	int Nskip = 100; //Number of iterations to skip
-	int Nend  = 200; //Number of total iterations
+ int main(){
+	double x0 = 0.5;
+	double x;
+ 	int Nskip = 100; //Number of iterations to skip
+ 	int Nend  = 200; //Number of total iterations
+
+	for(double r=0; r <= 4; r += 0.001){
 	double x0 = 0.5;
 	double x;
 	double xa[Nend-Nskip];
 	ofstream out("output.dat");
-	
+
 	for(double r=0; r <= 4; r += 0.01){
-	   x=x0;
-	   
-	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
-	   
-	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
+ 	   x=x0;
+
+ 	   for(int i=1; i <= Nskip; i++)
+ 		   x = r*x*(1-x);
+
+ 	   for(int i=Nskip+1; i <= Nend; i++){
+ 	   		   x = r*x*(1-x);
 			   xa[i-(Nskip+1)]=x;
-   	   }
+    	   }
    	   for(int i=0; i<=(Nend-Nskip-1); i++)
 	     out << r << "\t" << xa[i] << endl;
-	}
-	
-	out.close();
+ 	}
+out.close();
 
-	return 0;
+ 	return 0;
+ }
 }
